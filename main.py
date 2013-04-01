273b5fa6c6bfe3cc46243746a6aa5579a37b91e1
Kristian Barrett <kbarrett-86@hotmail.com>
2013-04-01 15:15:50 +0200

    Test from pycharm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import csv
 from pybrain.datasets import SupervisedDataSet
 
 DS = SupervisedDataSet(3, 2)
@@ -6,4 +7,13 @@ DS.appendLinked([1, 2, 3], [4, 5])
 sys.stdout.write("hello\n")
 sys.stdout.write(DS['input'])
 
-sys.stdout.closed
+class GreenEnergyDataSet(SupervisedDataSet):
+    """ A dataset for the XOR function."""
+    def __init__(self):
+        SupervisedDataSet.__init__(self, 2, 1)
+
+        with open('C:\Users\Brian\Desktop\Brian\Universitetet\Kandidat\Master Thesis\WeLoveGREEN-ENERGY\DATASET_FOR_GREEN_ENERGY_PLOTTING\wind_temp_production.csv', 'rb') as csvfile:
+            dat = csv.reader(csvfile, delimiter=';')
+            for row in dat:
+            #  print 'sample 0: ' + row[0] + ' sample 1: ' + row[1]
+                self.addSample([int(row[1]),int(row[2])],[int(row[0])])
