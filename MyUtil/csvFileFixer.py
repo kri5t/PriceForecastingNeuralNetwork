797de653a19bbc9853a3484cef18494e369894de
Kristian Barrett <kbarrett-86@hotmail.com>
2013-04-08 13:31:36 +0200

    Average over weekdays

---
+++ MyUtil/csvFileFixer.py
@@ -0,0 +1,161 @@
+import csv
+import sys
+
+
+class csvFileFixer():
+
+    delimiter = ''
+
+    def __init__(self, delimiter):
+        """ Init method """
+        self.delimiter = delimiter
+
+    def fixDateFormat(self, filePath, fileToSaveToPath, rowNumberToFix):
+        """Hand a file to it and it fixes the format"""
+        with open(fileToSaveToPath, 'wb') as writeFile:
+            with open(filePath, 'rU') as csvFile:
+                reader = csv.reader(csvFile, delimiter=self.delimiter)
+                writer = csv.writer(writeFile, delimiter=self.delimiter)
+                for row in reader:
+
+                    if len(row[rowNumberToFix]) > 8:
+                        row[rowNumberToFix] = str(row[rowNumberToFix])[3:5] + "/" + str(row[0])[0:2] + "/" + str(
+                            row[0])[8:10]
+                        if str(row[0])[0:1] == "0": row[rowNumberToFix] = str(row[rowNumberToFix])[1:]
+                    writer.writerow(row)
+
+    def copyRow(self, fromDocument, rowToCopy, toDocument, toWhatRow):
+        """Copy one row from one document to another document"""
+        with open(toDocument, 'wb') as writeFile:
+            with open(fromDocument, 'rU') as readFile:
+                reader = csv.reader(readFile, delimiter=self.delimiter)
+                writer = csv.writer(writeFile, delimiter=self.delimiter)
+                for row in reader:
+                    sys.stdout.write(str(len(row)))
+
+    def printCsvDocument(self, documentToPrint):
+        """Prints the specified CSV document"""
+        with open(documentToPrint, 'rU') as readFile:
+            reader = csv.reader(readFile, delimiter=self.delimiter)
+            indexPrinted = False
+            for row in reader:
+                if indexPrinted is False:
+                    sys.stdout.write("Number of rows in csvFile: " + str(len(row)) + "\n")
+                    indexPrinted = True
+
+                for index in range(len(row)):
+                    sys.stdout.write(row[index] + "\t\t")
+                sys.stdout.write("\n")
+
+    def cleanMinusAndNullInDocumentRow(self, document, toDocument, columns):
+        """Removes minus signs and Null values"""
+        flag = True
+        with open(toDocument, 'wb') as writeToFile:
+            with open(document, 'rU') as readFromFile:
+                reader = csv.reader(readFromFile, delimiter=self.delimiter)
+                writer = csv.writer(writeToFile, delimiter=self.delimiter)
+                skipFirstRow = True
+                for row in reader:
+                    if skipFirstRow is False:
+                        for r in columns:
+                            castToInt = str(row[r]).replace(",", ".").replace(" ", "")
+                            if "-" in castToInt or castToInt is "":
+                                flag = False
+                            else:
+                                row[r] = int(round(float(castToInt)))
+                        if flag:
+                            writer.writerow(row)
+                            flag = True
+                        else:
+                            flag = True
+
+                    else:
+                        skipFirstRow = False
+
+    def removeToHighAndToLow(self, document, toDocument, columns):
+        value = [len(columns)]
+        numberOfValues = [len(columns)]
+        medians = [len(columns)]
+        entries = 0
+        entriesWritten = 0
+        with open(toDocument, 'wb') as writeToFile:
+            with open(document, 'rU') as readFromFile:
+
+                reader = csv.reader(readFromFile, delimiter=self.delimiter)
+                writer = csv.writer(writeToFile, delimiter=self.delimiter)
+                for row in reader:
+                    entries += 1
+                    for index in range(len(columns)):
+                        if value[index] is None:
+                            value[index] = int(row[columns[index]])
+                        else:
+                            value[index] += int(row[columns[index]])
+                        numberOfValues[index] += 1
+                for index in range(len(value)):
+                    medians[index] = value[index] / numberOfValues[index]
+                readFromFile.seek(0)
+                reader = csv.reader(readFromFile, delimiter=self.delimiter)
+                for row in reader:
+                    for index in range(len(columns)):
+                        if int(medians[index] + medians[index]) > int(row[columns[index]]) > 0:
+                            writer.writerow(row)
+                            entriesWritten += 1
+        sys.stdout.write(str(entries - entriesWritten) + "\n")
+
+    def twoRowsToOneFile(self, document, toFile, row1, row2):
+        """Creates new CSV file with only 2 rows"""
+        with open(toFile, 'wb') as writeToFile:
+            with open(document, 'rU') as readFromFile:
+                reader = csv.reader(readFromFile, delimiter=self.delimiter)
+                writer = csv.writer(writeToFile, delimiter=self.delimiter)
+                for row in reader:
+                    writer.writerow([row[row1], row[row2]])
+
+    def concatenateWeekdaysAndGiveAverages(self, document, weekdayRow, priceRow):
+        numberOfWeekdays = [0, 0, 0, 0, 0, 0, 0]
+        weekdaysCombined = [0, 0, 0, 0, 0, 0, 0]
+        medianPricesForAllWeekdays = [0, 0, 0, 0, 0, 0, 0]
+        with open(document, 'rU') as readFromFile:
+            reader = csv.reader(readFromFile, delimiter=self.delimiter)
+            for row in reader:
+                if row[weekdayRow] == "Mon":
+                    numberOfWeekdays[0] += 1
+                    weekdaysCombined[0] += float(row[priceRow])
+                elif row[weekdayRow] == "Tue":
+                    numberOfWeekdays[1] += 1
+                    weekdaysCombined[1] += float(row[priceRow])
+                elif row[weekdayRow] == "Wed":
+                    numberOfWeekdays[2] += 1
+                    weekdaysCombined[2] += float(row[priceRow])
+                elif row[weekdayRow] == "Thu":
+                    numberOfWeekdays[3] += 1
+                    weekdaysCombined[3] += float(row[priceRow])
+                elif row[weekdayRow] == "Fri":
+                    numberOfWeekdays[4] += 1
+                    weekdaysCombined[4] += float(row[priceRow])
+                elif row[weekdayRow] == "Sat":
+                    numberOfWeekdays[5] += 1
+                    weekdaysCombined[5] += float(row[priceRow])
+                elif row[weekdayRow] == "Sun":
+                    numberOfWeekdays[6] += 1
+                    weekdaysCombined[6] += float(row[priceRow])
+                else:
+                    sys.stdout.write("found nothing")
+        for index in range(len(numberOfWeekdays)):
+            medianPricesForAllWeekdays[index] = weekdaysCombined[index] / numberOfWeekdays[index]
+            sys.stdout.write("median: " + str(medianPricesForAllWeekdays[index]) + "  weekdays: " + str(weekdaysCombined[index]) + "  numberOfWeekdays: " + str(numberOfWeekdays[index]) +"\n")
+        return medianPricesForAllWeekdays
+
+
+
+def main():
+    fixer = csvFileFixer()
+    filePath = 'DA_EXCEL_FOR_DA_PRICE_FORECAST.csv'
+    fileToSaveToPath = 'DA_EXCEL_FOR_DA_PRICE_FORECAST_FIXED.csv'
+
+    fixer.fixDateFormat(filePath, fileToSaveToPath, 0)
+    fixer.printCsvDocument('DA_EXCEL_FOR_DA_PRICE_FORECAST_FIXED.csv')
+
+
+if __name__ == '__main__':
+    main()
--- TemperatureAndPrice.py
+++ TemperatureAndPrice.py
@@ -4,7 +4,8 @@ from MyUtil.csvFileFixer import csvFileFixer
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.mlab as mlab
+import sys
+
 
 class graphPlotter():
 
@@ -15,8 +16,7 @@ class graphPlotter():
         self.delimiter = delimiter
 
     def plot(self, fromCsvFile, col1, col1Name, col2, col2Name):
-        #myData = mlab.csv2rec(fromCsvFile, delimiter=';', skiprows=1)
-        #myData.dump('/Users/kristian/Downloads/DUMP.csv')
+        """ Plots out the csvFile. Takes 2 columns and their names. """
         plt.plotfile(fromCsvFile, delimiter=self.delimiter, cols=(col1, col2), skiprows=1,
                      names=(col1Name, col2Name), linestyle='None', marker='.')
 
@@ -36,7 +36,15 @@ class graphPlotter():
 
 
 def main():
-    fileName = 'DA_EXCEL_FOR_DA_PRICE_FORECAST'
+    #fileName = 'csvFiles/DA_EXCEL_FOR_DA_PRICE_FORECAST'
+    #fileName1 = 'csvFiles/STATION_LIST_AVERAGE_COMBINING'
+    #filePath = fileName + '.csv'
+    #document = fileName + '_FIXED.csv'
+    #cleanedDocument = fileName + '_CLEANED.csv'
+    #twoRows = fileName + '_2ROWS.csv'
+    #correctedData = fileName + '_CORRECTED_DATA.csv'
+
+    fileName = 'csvFiles/PRICE_CONSUMPTION_WEATHER_COMBI'
     filePath = fileName + '.csv'
     #document = fileName + '_FIXED.csv'
     cleanedDocument = fileName + '_CLEANED.csv'
@@ -45,12 +53,45 @@ def main():
 
     fileFixer = csvFileFixer(",")
     #fileFixer.fixDateFormat(filePath, document, 0)
+
+    #Price vs Temperature
     fileFixer.cleanMinusAndNullInDocumentRow(filePath, cleanedDocument, [2, 4])
     fileFixer.printCsvDocument(cleanedDocument)
     fileFixer.twoRowsToOneFile(cleanedDocument, twoRows, 2, 4)
     fileFixer.removeToHighAndToLow(twoRows, correctedData, [0])
     gp = graphPlotter(",")
-    gp.plot(correctedData, 1, "DK1", 0, "Temperature")
+    gp.plot(correctedData, 1, "Price", 0, "Temperature")
 
+    #Price vs Wind speed
+    fileFixer.cleanMinusAndNullInDocumentRow(filePath, cleanedDocument, [3, 2])
+    fileFixer.twoRowsToOneFile(cleanedDocument, twoRows, 3, 4)
+    fileFixer.removeToHighAndToLow(twoRows, correctedData, [1])
+    gp2 = graphPlotter(",")
+    gp2.plot(correctedData, 0, "Wind speed", 1, "Price")
+
+    #Price vs Consumption
+    #fileFixer.fixDateFormat(filePath1, document1, 0)
+    fileFixer.cleanMinusAndNullInDocumentRow(filePath, cleanedDocument, [2, 5])
+    fileFixer.twoRowsToOneFile(cleanedDocument, twoRows, 2, 5)
+    fileFixer.removeToHighAndToLow(twoRows, correctedData, [0])
+    gp1 = graphPlotter(",")
+    gp1.plot(correctedData, 0, "Price", 1, "Consumption")
+
+    #Temperature vs Consumption
+    fileFixer.cleanMinusAndNullInDocumentRow(filePath, cleanedDocument, [4, 5])
+    fileFixer.twoRowsToOneFile(cleanedDocument, twoRows, 4, 5)
+    #fileFixer.removeToHighAndToLow(twoRows, correctedData, [0])
+    gp2 = graphPlotter(",")
+    gp2.plot(twoRows, 0, "Temperature", 1, "Consumption")
+
+    #Wind speed vs Consumption
+    fileFixer.cleanMinusAndNullInDocumentRow(filePath, cleanedDocument, [3, 5])
+    fileFixer.twoRowsToOneFile(cleanedDocument, twoRows, 3, 5)
+    fileFixer.removeToHighAndToLow(twoRows, correctedData, [1])
+    gp2 = graphPlotter(",")
+    gp2.plot(correctedData, 0, "Wind speed", 1, "Consumption")
+
+    fileFixer.cleanMinusAndNullInDocumentRow(filePath, cleanedDocument, [2])
+    fileFixer.concatenateWeekdaysAndGiveAverages(cleanedDocument, 6, 2)
 if __name__ == '__main__':
     main()
